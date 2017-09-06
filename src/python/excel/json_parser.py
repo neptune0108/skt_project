41052ec7fbed47c985022ef3ce7b18800fbeda6c
neptune0108 <30815166+neptune0108@users.noreply.github.com>
2017-09-06 09:50:11 +0900

    Update json_parser.py

--- src/python/excel/json_parser.py
+++ src/python/excel/json_parser.py
@@ -11,25 +11,26 @@ def get_json_data(file_directory):
 
 # print(len(data['ta_analysis_result']))
 
+    total_length = len(data['ta_analysis_result']['file_list'])
     max_keyword_set_length = 0
     max_sentence_keyword_set_length = 0
 
     json_string_list = []
 
-    for ta_tmp in data['ta_analysis_result']:
+    for i in range(total_length):
         #    print(ta_tmp['file_name'])
         #    print(ta_tmp['service_number'])
         #    print(ta_tmp['date'])
         #    print(ta_tmp['type'])
 
-        file_name = ta_tmp['file_name']
-        service_number = ta_tmp['service_number']
-        date = ta_tmp['date']
-        type_info = ta_tmp['type']
+        file_name = data['ta_analysis_result']['file_list'][i]
+        service_number = file_name
+        date = '2017-09-04'
+        type_info = ''
 
     #    print(max(len(l) for l in ta_tmp['paragraph']['keyword_set'))
 
-        for paragraph_tmp in ta_tmp['paragraph']:
+        for paragraph_tmp in data['ta_analysis_result']['paragraph_list'][i]:
             main_topic = paragraph_tmp['topic']
             sub_topic = paragraph_tmp['sub_topic']
             keyword = paragraph_tmp['keyword']
@@ -42,6 +43,8 @@ def get_json_data(file_directory):
             string_list.append(service_number)
             string_list.append(date)
             string_list.append(type_info)
+            string_list.append(main_topic)
+            string_list.append(sub_topic)
             string_list.append(keyword)
             string_list.append(keyword_set)
             string_list.append(sentence_keyword_set)
